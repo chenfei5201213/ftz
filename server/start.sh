1e637d135db57035a16beb71b9c1925825ab893a
chenfei <chenfei@kuaishou.com>
2024-10-19 11:13:03 +0800

    增加超时、优雅重启机制

--- server/start.sh
+++ server/start.sh
@@ -4,7 +4,7 @@ if [[ "$DJANGO_ENV" == 'prod' ]]; then
     python manage.py collectstatic --noinput
     celery -A server worker -l info&
     celery -A server beat -l info&
-    gunicorn server.wsgi:application -w 4 -k gthread -b 0.0.0.0:8000
+    gunicorn server.wsgi:application -w 4 -k gthread -b 0.0.0.0:8000 --timeout 30 --graceful-timeout 120
 
     else
     python manage.py makemigrations
@@ -12,5 +12,5 @@ if [[ "$DJANGO_ENV" == 'prod' ]]; then
     celery -A server worker -l info&
     celery -A server beat -l info&
 #    python manage.py runserver 0.0.0.0:8000
-    gunicorn server.wsgi:application -w 1 -k gthread -b 0.0.0.0:8000
+    gunicorn server.wsgi:application -w 1 -k gthread -b 0.0.0.0:8000 --timeout 30 --graceful-timeout 120
 fi
